deb7806e537b3e2476bc7ab708e933f65f57e396
Matthew Morgan <matt4byu@gmail.com>
2020-10-30 16:00:55 -0600

    My start

---
+++ Matt_script.R
@@ -0,0 +1,11 @@
+#####################
+### Matt's Script ###
+#####################
+
+# Reading Libraries
+library(caret)
+library(tidyverse)
+
+# Reading in Data
+test <- read_csv('test.csv')
+train <- read_csv('train.csv')
