f685140c339ccfffd9bd50988bc9fc38e16fb122
Matthew Morgan <matt4byu@gmail.com>
2020-11-04 15:58:31 -0700

    Starting prophet

--- Matt_script.R
+++ Matt_script.R
@@ -4,8 +4,22 @@
 
 # Reading Libraries
 library(caret)
+library(DataExplorer)
+library(forecast)
+library(lubridate)
 library(tidyverse)
+library(zoo)
 
 # Reading in Data
-test <- read_csv('test.csv')
-train <- read_csv('train.csv')
+test <- read_csv('test.csv') %>% 
+  mutate(day = weekdays(date),
+         year = year(date),
+         yearmon = as.yearmon(date))
+
+train <- read_csv('train.csv') %>% 
+  mutate(day = weekdays(date),
+         year = year(date),
+         yearmon = as.yearmon(date))
+
+
+
